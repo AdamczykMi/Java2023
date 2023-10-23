c0e0a90df4611c59c3ab00a714a55576cd7796cb
michal <mi.adamczyk@student.uj.edu.pl>
2023-10-23 17:01:46 +0200

    LeapYearCalculator

---
+++ src/main/java/org/example/LeapYearCalculator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class LeapYearCalculator {
+    public static boolean isLapYear(int year){
+        if (year < 1 || year > 9999){
+            return false;
+        } else {
+            if (year % 4 == 0){
+                if (year % 100 == 0){
+                    if (year % 400 == 0){
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(isLapYear(400));
+    }
+}
