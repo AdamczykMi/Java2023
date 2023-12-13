69d66bf46155263507544698eeaec23892cea6d7
michal <mi.adamczyk@student.uj.edu.pl>
2023-12-13 15:59:14 +0100

    test01 fix of tests

--- src/test/java/org/projectmanagement/ProjectManagerTest.java
+++ src/test/java/org/projectmanagement/ProjectManagerTest.java
@@ -2,6 +2,7 @@ package org.projectmanagement;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.projectmanagement.*;
 
 import java.util.Date;
 import java.util.List;
